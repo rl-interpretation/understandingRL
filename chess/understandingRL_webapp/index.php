031080f4c4a493b191026a191ad76207495adf52
rl-interpretation <rl.interpretation@gmail.com>
2019-07-10 15:35:19 +0530

    added tactics to app

--- chess/understandingRL_webapp/index.php
+++ chess/understandingRL_webapp/index.php
@@ -9,6 +9,7 @@
     <script src="js/chessboard-1.0.0.min.js"></script>
     <script src="js/chessboard-1.0.0.js"></script>
     <script src="js/chess.js"></script>
+    <script src="js/pgns.js"></script>
     <script src="js/chess.min.js"></script>
 </head>
 
@@ -128,7 +129,10 @@
 			function onSnapEnd () {
 				board.position(game.fen())
 			}
-			var show_saliency = (Math.random() > 0.5)
+			// var show_saliency = (Math.random() > 0.5)
+			var tempDate = new Date();
+			var temp = tempDate.getTime();
+			var show_saliency = (temp%2 == 0); 
 			if(show_saliency){
 				alert("Play the tactic for white on the board on the left. You can take help from the board on the right which highlights important pieces for the tactic. Time will start when you click on OK.");
 			}
@@ -136,259 +140,7 @@
 				alert("Play the tactic for white on the board on the left. Time will start when you click on OK.")
 			}
 
-			var pgn = ['[Event "Tactic"]',
-			'[Site "Earth"]',
-			'[Date "2019.??.??"]',
-			'[EventDate "?"]',
-			'[Round "?"]',
-			'[Result "0-0"]',
-			'[White "You"]',
-			'[Black "Opponent"]',
-			'[FEN "2r1r1k1/b4ppp/p3p3/Pp2Nq2/1Pbp1B2/R7/2PQ1PP1/4R1K1 w - - 0 1"]',
-			'[SetUp "1"]',
-			'',
-			'1. g4 Qf6 2. Bg5 Qxg5 3. Qxg5'];
-		
-			var list_of_fens = [];
-			var list_of_pgns = [];
-
-			list_of_fens.push('2r1r1k1/b4ppp/p3p3/Pp2Nq2/1Pbp1B2/R7/2PQ1PP1/4R1K1 w - - 0 1');
-			list_of_fens.push('rnbq1rk1/pp2bppp/4p3/3p3n/3P1B2/3B1N2/PPPNQPPP/R3K2R w KQkq - 0 1');
-			list_of_fens.push('rnbqk1nr/1p3ppp/4p3/2bp4/8/p3PN2/1BP2PPP/RN1QKB1R w - - 0 1');
-			list_of_fens.push('1k3r2/p6p/2p1Bp2/4p3/1b1pP3/3P2P1/P1K4P/5R2 w - - 0 1');
-			list_of_fens.push('rn1qk1nr/p1pp1ppp/1pb1p3/8/2BQ4/2P2N2/PPP2PPP/R1B2RK1 w - - 0 1');
-			list_of_fens.push('r1r5/2k3pp/2p2p2/1pR1pq2/PQ1n4/3P4/1P4PP/1KRB4 w - - 0 1');
-			list_of_fens.push('r2r4/1kp2p1p/1q2b1p1/R7/Q7/P1N5/1PP4P/1K5R w - - 0 1');
-			list_of_fens.push('4r1k1/1R4p1/4qp1p/8/3r4/1Q3R1P/6P1/7K w - - 0 1');
-			list_of_fens.push('2kr3r/bbqp1pp1/p3p3/1p2n3/1P1NPBnp/P2B2QP/2P1NPP1/R1R3K1 w - - 0 1');
-			list_of_fens.push('1r6/pkp2p1p/5b2/5p2/5P2/q1PP4/2R1N2P/1K4Q1 w - - 0 1');
-			list_of_fens.push('1kr5/pr4p1/5n1p/5p2/3P4/qbR2P1P/B2Q1NP1/K6R w - - 0 1');
-			list_of_fens.push('1kr2b1r/1pq2bpp/p3np2/2p1p3/4P3/2N1BPN1/PPPR1QPP/1K1R4 w - - 0 1');
-			list_of_fens.push('5rk1/p1q2ppp/1p2p1n1/2p1r2Q/2P5/3B4/PP3PPP/3RR1K1 w - - 0 1');
-			list_of_fens.push('4rrk1/pp1qp1bp/2pnbpp1/8/3PNP2/2PB4/PP2Q1PP/R1B2RK1 w - - 0 1');
-			list_of_fens.push('2b1r1k1/2q1bppp/2p3n1/r1B1p3/N3n3/5N2/P3BPPP/2RQ1RK1 w - - 0 1');
-
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "2r1r1k1/b4ppp/p3p3/Pp2Nq2/1Pbp1B2/R7/2PQ1PP1/4R1K1 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. g4 Qf6 2. Bg5 Qxe5 3. Rxe5'];
-
-			list_of_pgns.push(pgn);	
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "rnbq1rk1/pp2bppp/4p3/3p3n/3P1B2/3B1N2/PPPNQPPP/R3K2R w KQkq - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Bxb8 Rxb8 2. Qe5 Nf4 3. Qxf4'];
-
-
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "rnbqk1nr/1p3ppp/4p3/2bp4/8/p3PN2/1BP2PPP/RN1QKB1R w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Bxg7 a2 2. Nbd2 Nf6 3. Bxh8'];
-
-
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "1k3r2/p6p/2p1Bp2/4p3/1b1pP3/3P2P1/P1K4P/5R2 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Rb1 a5 2. a3 Kc7 3. axb4'];
-
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "rn1qk1nr/p1pp1ppp/1pb1p3/8/2BQ4/2P2N2/PPP2PPP/R1B2RK1 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Qxg7 Qf6 2. Bh6 Qxh6 3. Qxh8'];
-
-
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "r1r5/2k3pp/2p2p2/1pR1pq2/PQ1n4/3P4/1P4PP/1KRB4 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Qxd4 exd4 2. Rxf5'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "r2r4/1kp2p1p/1q2b1p1/R7/Q7/P1N5/1PP4P/1K5R w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Qe4+ c6 2. Rb5 Qxb5 3. Nxb5'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "4r1k1/1R4p1/4qp1p/8/3r4/1Q3R1P/6P1/7K w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Re3 Qxb3 2. Rxe8+ Kh7 3. Rxb3'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "2kr3r/bbqp1pp1/p3p3/1p2n3/1P1NPBnp/P2B2QP/2P1NPP1/R1R3K1 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Qxg4 Nxg4 2. Bxc7 Kxc7 3. hxg4'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "1r6/pkp2p1p/5b2/5p2/5P2/q1PP4/2R1N2P/1K4Q1 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Rb2+ Kc8 2. Qg8+ Bd8 3. Rxb8+ Kxb8 4. Qxd8+'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "1kr5/pr4p1/5n1p/5p2/3P4/qbR2P1P/B2Q1NP1/K6R w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Qf4+ Rcc7 2. Qxc7+ Rxc7 3. Rxb3+ Qxb3 4. Bxb3'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "1kr2b1r/1pq2bpp/p3np2/2p1p3/4P3/2N1BPN1/PPPR1QPP/1K1R4 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Rd7 Qc6 2. Rxf7'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "5rk1/p1q2ppp/1p2p1n1/2p1r2Q/2P5/3B4/PP3PPP/3RR1K1 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Rxe5 Qxe5 2. Bxg6 Qxh5 3. Bxh5'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "4rrk1/pp1qp1bp/2pnbpp1/8/3PNP2/2PB4/PP2Q1PP/R1B2RK1 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Nc5 Bg4 2. Nxd7 Bxe2 3. Bxe2'];
-			list_of_pgns.push(pgn);
-
-			pgn = ['[Event "Tactic"]',
-				'[Site "Earth"]',
-				'[Date "2019.??.??"]',
-				'[EventDate "?"]',
-				'[Round "?"]',
-				'[Result "0-0"]',
-				'[White "You"]',
-				'[Black "Opponent"]',
-				'[FEN "2b1r1k1/2q1bppp/2p3n1/r1B1p3/N3n3/5N2/P3BPPP/2RQ1RK1 w - - 0 1"]',
-				'[SetUp "1"]',
-				'',
-				'1. Bb6 Qb7 2. Bxa5'];
-			list_of_pgns.push(pgn);
-
-
-			var idx = Math.floor(Math.random() * 15);
+			var idx = Math.floor(Math.random() * 29);
 
 			if(show_saliency) {
 				document.getElementById('map').innerHTML = '<img src="maps/' + idx + '.png">'
@@ -447,11 +199,6 @@
 			// 	board.position(game.fen());
 			// 	i = history.length;
 			// });
-			
-
-
-
-
 		});
 	</script>
 </body>
---
+++ chess/understandingRL_webapp/js/pgns.js
@@ -0,0 +1,515 @@
+var pgn = ['[Event "Tactic"]',
+			'[Site "Earth"]',
+			'[Date "2019.??.??"]',
+			'[EventDate "?"]',
+			'[Round "?"]',
+			'[Result "0-0"]',
+			'[White "You"]',
+			'[Black "Opponent"]',
+			'[FEN "2r1r1k1/b4ppp/p3p3/Pp2Nq2/1Pbp1B2/R7/2PQ1PP1/4R1K1 w - - 0 1"]',
+			'[SetUp "1"]',
+			'',
+			'1. g4 Qf6 2. Bg5 Qxg5 3. Qxg5'];
+		
+			var list_of_fens = [];
+			var list_of_pgns = [];
+
+			list_of_fens.push('2r1r1k1/b4ppp/p3p3/Pp2Nq2/1Pbp1B2/R7/2PQ1PP1/4R1K1 w - - 0 1');
+			list_of_fens.push('rnbq1rk1/pp2bppp/4p3/3p3n/3P1B2/3B1N2/PPPNQPPP/R3K2R w KQkq - 0 1');
+			list_of_fens.push('rnbqk1nr/1p3ppp/4p3/2bp4/8/p3PN2/1BP2PPP/RN1QKB1R w - - 0 1');
+			list_of_fens.push('1k3r2/p6p/2p1Bp2/4p3/1b1pP3/3P2P1/P1K4P/5R2 w - - 0 1');
+			list_of_fens.push('rn1qk1nr/p1pp1ppp/1pb1p3/8/2BQ4/2P2N2/PPP2PPP/R1B2RK1 w - - 0 1');
+			list_of_fens.push('r1r5/2k3pp/2p2p2/1pR1pq2/PQ1n4/3P4/1P4PP/1KRB4 w - - 0 1');
+			list_of_fens.push('r2r4/1kp2p1p/1q2b1p1/R7/Q7/P1N5/1PP4P/1K5R w - - 0 1');
+			list_of_fens.push('4r1k1/1R4p1/4qp1p/8/3r4/1Q3R1P/6P1/7K w - - 0 1');
+			list_of_fens.push('2kr3r/bbqp1pp1/p3p3/1p2n3/1P1NPBnp/P2B2QP/2P1NPP1/R1R3K1 w - - 0 1');
+			list_of_fens.push('1r6/pkp2p1p/5b2/5p2/5P2/q1PP4/2R1N2P/1K4Q1 w - - 0 1');
+			list_of_fens.push('1kr5/pr4p1/5n1p/5p2/3P4/qbR2P1P/B2Q1NP1/K6R w - - 0 1');
+			list_of_fens.push('1kr2b1r/1pq2bpp/p3np2/2p1p3/4P3/2N1BPN1/PPPR1QPP/1K1R4 w - - 0 1');
+			list_of_fens.push('5rk1/p1q2ppp/1p2p1n1/2p1r2Q/2P5/3B4/PP3PPP/3RR1K1 w - - 0 1');
+			list_of_fens.push('4rrk1/pp1qp1bp/2pnbpp1/8/3PNP2/2PB4/PP2Q1PP/R1B2RK1 w - - 0 1');
+			list_of_fens.push('2b1r1k1/2q1bppp/2p3n1/r1B1p3/N3n3/5N2/P3BPPP/2RQ1RK1 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "2r1r1k1/b4ppp/p3p3/Pp2Nq2/1Pbp1B2/R7/2PQ1PP1/4R1K1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. g4 Qf6 2. Bg5 Qxe5 3. Rxe5'];
+
+			list_of_pgns.push(pgn);	
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "rnbq1rk1/pp2bppp/4p3/3p3n/3P1B2/3B1N2/PPPNQPPP/R3K2R w KQkq - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Bxb8 Rxb8 2. Qe5 Nf4 3. Qxf4'];
+
+
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "rnbqk1nr/1p3ppp/4p3/2bp4/8/p3PN2/1BP2PPP/RN1QKB1R w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Bxg7 a2 2. Nbd2 Nf6 3. Bxh8'];
+
+
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "1k3r2/p6p/2p1Bp2/4p3/1b1pP3/3P2P1/P1K4P/5R2 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Rb1 a5 2. a3 Kc7 3. axb4'];
+
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "rn1qk1nr/p1pp1ppp/1pb1p3/8/2BQ4/2P2N2/PPP2PPP/R1B2RK1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Qxg7 Qf6 2. Bh6 Qxh6 3. Qxh8'];
+
+
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "r1r5/2k3pp/2p2p2/1pR1pq2/PQ1n4/3P4/1P4PP/1KRB4 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Qxd4 exd4 2. Rxf5'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "r2r4/1kp2p1p/1q2b1p1/R7/Q7/P1N5/1PP4P/1K5R w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Qe4+ c6 2. Rb5 Qxb5 3. Nxb5'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "4r1k1/1R4p1/4qp1p/8/3r4/1Q3R1P/6P1/7K w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Re3 Qxb3 2. Rxe8+ Kh7 3. Rxb3'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "2kr3r/bbqp1pp1/p3p3/1p2n3/1P1NPBnp/P2B2QP/2P1NPP1/R1R3K1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Qxg4 Nxg4 2. Bxc7 Kxc7 3. hxg4'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "1r6/pkp2p1p/5b2/5p2/5P2/q1PP4/2R1N2P/1K4Q1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Rb2+ Kc8 2. Qg8+ Bd8 3. Rxb8+ Kxb8 4. Qxd8+'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "1kr5/pr4p1/5n1p/5p2/3P4/qbR2P1P/B2Q1NP1/K6R w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Qf4+ Rcc7 2. Qxc7+ Rxc7 3. Rxb3+ Qxb3 4. Bxb3'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "1kr2b1r/1pq2bpp/p3np2/2p1p3/4P3/2N1BPN1/PPPR1QPP/1K1R4 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Rd7 Qc6 2. Rxf7'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "5rk1/p1q2ppp/1p2p1n1/2p1r2Q/2P5/3B4/PP3PPP/3RR1K1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Rxe5 Qxe5 2. Bxg6 Qxh5 3. Bxh5'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "4rrk1/pp1qp1bp/2pnbpp1/8/3PNP2/2PB4/PP2Q1PP/R1B2RK1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Nc5 Bg4 2. Nxd7 Bxe2 3. Bxe2'];
+			list_of_pgns.push(pgn);
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "2b1r1k1/2q1bppp/2p3n1/r1B1p3/N3n3/5N2/P3BPPP/2RQ1RK1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Bb6 Qb7 2. Bxa5'];
+			list_of_pgns.push(pgn);
+
+
+			list_of_fens.push('4bn2/8/3K2k1/5pp1/8/4PP1N/1B4P1/8 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "4bn2/8/3K2k1/5pp1/8/4PP1N/1B4P1/8 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Ke7 Bb5 2. Kxf8'];
+
+			list_of_pgns.push(pgn);	
+
+			list_of_fens.push('7R/8/8/pp2PpB1/3K4/Pb4k1/1P6/4r3 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "7R/8/8/pp2PpB1/3K4/Pb4k1/1P6/4r3 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Bh4+ Kg4 2. Bxe1'];
+
+			list_of_pgns.push(pgn);	
+
+
+			list_of_fens.push('3Rr1k1/pp3p2/8/2p1r2p/2P4p/P1Q1PqP1/1P3P2/5RK1 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "3Rr1k1/pp3p2/8/2p1r2p/2P4p/P1Q1PqP1/1P3P2/5RK1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Qxe5 Rxd8 2. Qg5+ Kh7 3. Qxd8'];
+
+			list_of_pgns.push(pgn);	
+
+			list_of_fens.push('3rk2r/1bq2ppp/p1pbpn2/1pn5/4P3/2N1BN1P/PPB2PP1/R1Q2RK1 w k - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "3rk2r/1bq2ppp/p1pbpn2/1pn5/4P3/2N1BN1P/PPB2PP1/R1Q2RK1 w k - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. e5 Bxe5 2. Bxc5'];
+
+			list_of_pgns.push(pgn);	
+
+
+			list_of_fens.push('r1b1Qqk1/pp6/2p4p/4n1p1/2P5/3B2N1/PP3P2/6K1 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "r1b1Qqk1/pp6/2p4p/4n1p1/2P5/3B2N1/PP3P2/6K1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Bh7+ Kg7 2. Nh5+ Kxh7 3. Qxf8'];
+
+			list_of_pgns.push(pgn);	
+
+
+			list_of_fens.push('3k4/pp2bp1p/1q2pnr1/2pb4/7Q/1NB1p3/PPP2PPP/1K3B1R w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "3k4/pp2bp1p/1q2pnr1/2pb4/7Q/1NB1p3/PPP2PPP/1K3B1R w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Ba5 Qxa5 2. Nxa5'];
+
+			list_of_pgns.push(pgn);	
+
+			list_of_fens.push('6k1/1p4q1/p2p2r1/P2Pp1b1/1P2PpQn/6nN/3BB1P1/2R3K1 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "6k1/1p4q1/p2p2r1/P2Pp1b1/1P2PpQn/6nN/3BB1P1/2R3K1 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Rc8+ Kh7 2. Nxg5+ Rxg5 3. Qxh4+'];
+
+			list_of_pgns.push(pgn);	
+
+
+			list_of_fens.push('r3rk2/p3qppp/5nb1/1N1pb3/8/1P1P1P2/PBP4P/1KR1Q2R w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "r3rk2/p3qppp/5nb1/1N1pb3/8/1P1P1P2/PBP4P/1KR1Q2R w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Ba3 Qxa3 2. Nxa3'];
+
+			list_of_pgns.push(pgn);	
+
+
+			list_of_fens.push('8/pp6/3pkp2/3R1Np1/1nP5/5P2/PP2r1PP/1K6 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "8/pp6/3pkp2/3R1Np1/1nP5/5P2/PP2r1PP/1K6 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Nd4+ Kf7 2. Nxe2 Nxd5 3. cxd5'];
+
+			list_of_pgns.push(pgn);	
+
+
+
+			list_of_fens.push('2k5/p7/1p1PK3/4B2p/8/7r/PP6/8 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "2k5/p7/1p1PK3/4B2p/8/7r/PP6/8 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. d7+ Kb7 2. d8=Q'];
+
+			list_of_pgns.push(pgn);	
+
+
+			list_of_fens.push('1kr4r/pb1n1ppp/1p1qpn2/2pp4/5P2/PP1PPN2/1BP3PP/1KR1QB1R w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "1kr4r/pb1n1ppp/1p1qpn2/2pp4/5P2/PP1PPN2/1BP3PP/1KR1QB1R w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Be5 Nxe5 2. fxe5 Qe7 3. exf6'];
+
+			list_of_pgns.push(pgn);	
+
+
+			list_of_fens.push('r2k3r/pbp2ppp/1pn1p3/4n3/2P1B3/4PN2/P1PB1PPP/R3RK2 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "r2k3r/pbp2ppp/1pn1p3/4n3/2P1B3/4PN2/P1PB1PPP/R3RK2 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Nxe5 Nxe5 2. Bxb7'];
+
+			list_of_pgns.push(pgn);	
+
+
+			list_of_fens.push('1kq4r/2pQn1pp/5b2/B1Np4/8/4PP1P/PrP5/2K2R2 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "1kq4r/2pQn1pp/5b2/B1Np4/8/4PP1P/PrP5/2K2R2 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Bxc7+ Qxc7 2. Na6+ Ka7 3. Nxc7'];
+
+			list_of_pgns.push(pgn);	
+
+
+
+			list_of_fens.push('3R4/1k2rp2/p1q3p1/1p4P1/2p5/P7/1PP3Q1/1K6 w - - 0 1');
+
+
+			pgn = ['[Event "Tactic"]',
+				'[Site "Earth"]',
+				'[Date "2019.??.??"]',
+				'[EventDate "?"]',
+				'[Round "?"]',
+				'[Result "0-0"]',
+				'[White "You"]',
+				'[Black "Opponent"]',
+				'[FEN "3R4/1k2rp2/p1q3p1/1p4P1/2p5/P7/1PP3Q1/1K6 w - - 0 1"]',
+				'[SetUp "1"]',
+				'',
+				'1. Rb8+ Kxb8 2. Qxc6'];
+
+			list_of_pgns.push(pgn);	
